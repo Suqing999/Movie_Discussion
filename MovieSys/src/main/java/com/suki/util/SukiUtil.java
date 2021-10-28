405d4335c9b92c879981755a9475ad65af7bdee0
Suki <1162457366@qq.com>
2021-10-28 21:59:52 +0800

    first

---
+++ MovieSys/src/main/java/com/suki/util/SukiUtil.java
@@ -0,0 +1,45 @@
+package com.suki.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SukiUtil {
+
+    /**
+     * 获得Md5加密
+     *
+     * @param str 原字符串
+     * @return 加密后的字符串
+     */
+    public static String strToMd5(String str) {
+        String md5Str = null;
+        if (str != null && str.length() != 0) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("MD5");
+                md.update(str.getBytes());
+                byte b[] = md.digest();
+
+                int i;
+                StringBuffer buf = new StringBuffer("");
+                for (int offset = 0; offset < b.length; offset++) {
+                    i = b[offset];
+                    if (i < 0) {
+                        i += 256;
+                    }
+                    if (i < 16) {
+                        buf.append("0");
+                    }
+                    buf.append(Integer.toHexString(i));
+                }
+                //32位
+                md5Str = buf.toString();
+                //16位
+                //md5Str = buf.toString().substring(8, 24);
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+        }
+        return md5Str;
+    }
+
+}
---
+++ out/artifacts/MovieSys_war_exploded/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: suki
+  Date: 2021/10/28
+  Time: 21:17
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>first</title>
+  </head>
+  <body>
+    搭建完成
+  </body>
+</html>
